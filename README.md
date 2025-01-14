d530d39b8522111e939b5e9c36b0544a1755947c
ekdldksp123 <ekdldksp123@naver.com>
2025-01-15 03:17:32 +0900

    :tada: project setting

---
+++ README.md
@@ -0,0 +1,5 @@
+# 오픈소스컨설팅 FE 과제
+
+- React 16.8
+- Typescript 4.5.5
+- Parcel 1.12.4
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { render } from "react-dom"
+import TodoListClass from "./components/example/class"
+import Provider from "./Provider"
+import "@atlaskit/css-reset"
+
+const App = () => {
+  return (
+    <Provider>
+      <TodoListClass />
+    </Provider>
+  )
+}
+
+render(<App />, document.getElementById("root"))
