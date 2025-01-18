16539616b70ee16bd8fadb753e7b772b079bb368
ekdldksp123 <ekdldksp123@naver.com>
2025-01-18 21:04:23 +0900

    :lipstick: update edit ui

--- src/components/post/CommentList.tsx
+++ src/components/post/CommentList.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Fragment, useMemo, useState } from "react"
 import Comment, {
   CommentAction,
   CommentAuthor,
@@ -9,13 +9,44 @@ import Avatar from "@atlaskit/avatar"
 import { useTranslation } from "react-i18next"
 import { v4 as uuidv4 } from "uuid"
 import { Grid } from "@atlaskit/primitives"
+import Form, { ErrorMessage, Field } from "@atlaskit/form"
+import TextArea from "@atlaskit/textarea"
 
 interface CommentListProps {
   comments: CommentType[]
 }
 
+type CommentFormType = {
+  content: string
+}
+
 export default ({ comments }: CommentListProps) => {
   const { t } = useTranslation("post")
+  const [isEditMode, setIsEditMode] = useState<boolean>(false)
+
+  const actions = useMemo(
+    () =>
+      isEditMode
+        ? [
+            <CommentAction
+              appearance="primary"
+              onClick={() => setIsEditMode(true)}
+            >
+              {t("confirm")}
+            </CommentAction>,
+            <CommentAction onClick={() => setIsEditMode(false)}>
+              {t("cancel")}
+            </CommentAction>
+          ]
+        : [
+            <CommentAction onClick={() => setIsEditMode(true)}>
+              {t("update")}
+            </CommentAction>,
+            <CommentAction>{t("delete")}</CommentAction>
+          ],
+    [isEditMode]
+  )
+
   return (
     <Grid>
       {comments.map(({ userId, createdAt, content }) => (
@@ -24,12 +55,33 @@ export default ({ comments }: CommentListProps) => {
           avatar={<Avatar name={`${userId}`} />}
           author={<CommentAuthor>{userId}</CommentAuthor>}
           time={<CommentTime>{createdAt}</CommentTime>}
-          content={<p>{content}</p>}
-          actions={[
-            <CommentAction>{t("update")}</CommentAction>,
-            <CommentAction>{t("delete")}</CommentAction>
-          ]}
-        ></Comment>
+          content={
+            isEditMode ? (
+              <Form<CommentFormType> onSubmit={(data) => console.log(data)}>
+                {({ formProps, submitting }) => (
+                  <form {...formProps}>
+                    <Field
+                      name="content"
+                      label={t("content")}
+                      defaultValue={content}
+                      isRequired
+                    >
+                      {({ fieldProps, error }) => (
+                        <Fragment>
+                          <TextArea {...fieldProps} />
+                          {error && <ErrorMessage>{error}</ErrorMessage>}
+                        </Fragment>
+                      )}
+                    </Field>
+                  </form>
+                )}
+              </Form>
+            ) : (
+              <p>{content}</p>
+            )
+          }
+          actions={actions}
+        />
       ))}
     </Grid>
   )
--- src/components/post/PostDetail.tsx
+++ src/components/post/PostDetail.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Fragment, useState } from "react"
 
 import Heading from "@atlaskit/heading"
 import { Flex, Grid } from "@atlaskit/primitives"
@@ -10,13 +10,23 @@ import Page, { GridColumn } from "@atlaskit/page"
 import { ButtonGroup } from "@atlaskit/button"
 import { useTranslation } from "react-i18next"
 import Button from "@atlaskit/button"
+import TextField from "@atlaskit/textfield"
+import Form, { ErrorMessage, Field } from "@atlaskit/form"
+import TextArea from "@atlaskit/textarea"
 
 interface PostDetailProps {
   postId: string
 }
 
+type PostFormType = {
+  title: string
+  content: string
+}
+
 export default ({ postId }: PostDetailProps) => {
   const searchId = Number(postId)
+
+  const [isEditMode, setIsEditMode] = useState<boolean>(false)
 
   const { data, isLoading } = useSinglePostQuery({
     searchId,
@@ -45,19 +55,75 @@ export default ({ postId }: PostDetailProps) => {
     <Page>
       <Grid gap="space.500">
         <Flex direction="column" gap="space.100">
-          <Flex gap="space.300">
-            <Heading size="medium" as="h1">
-              {post?.title}
-            </Heading>
-            <span>{post?.createdAt}</span>
-          </Flex>
+          {isEditMode ? (
+            <Form<PostFormType> onSubmit={(data) => console.log(data)}>
+              {({ formProps, submitting }) => (
+                <form {...formProps}>
+                  <Field
+                    name="title"
+                    label={t("title")}
+                    defaultValue={post?.title}
+                    isRequired
+                  >
+                    {({ fieldProps, error }) => (
+                      <TextField autoComplete="off" {...fieldProps} />
+                    )}
+                  </Field>
+                  <Field
+                    name="content"
+                    label={t("content")}
+                    defaultValue={post?.content}
+                    isRequired
+                  >
+                    {({ fieldProps, error }) => (
+                      <Fragment>
+                        <TextArea {...fieldProps} />
+                        {error && <ErrorMessage>{error}</ErrorMessage>}
+                      </Fragment>
+                    )}
+                  </Field>
+                </form>
+              )}
+            </Form>
+          ) : (
+            <>
+              <Flex gap="space.300">
+                <Heading size="medium" as="h1">
+                  {post?.title}
+                </Heading>
 
-          <p>{post?.content}</p>
+                <span>{post?.createdAt}</span>
+              </Flex>
+
+              <p>{post?.content}</p>
+            </>
+          )}
 
           <GridColumn>
             <ButtonGroup>
-              <Button appearance="primary">{t("update")}</Button>
-              <Button>{t("delete")}</Button>
+              {isEditMode ? (
+                <>
+                  <Button
+                    appearance="primary"
+                    onClick={() => setIsEditMode(true)}
+                  >
+                    {t("confirm")}
+                  </Button>
+                  <Button onClick={() => setIsEditMode(false)}>
+                    {t("cancel")}
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    appearance="primary"
+                    onClick={() => setIsEditMode(true)}
+                  >
+                    {t("update")}
+                  </Button>
+                  <Button>{t("delete")}</Button>
+                </>
+              )}
             </ButtonGroup>
           </GridColumn>
         </Flex>
